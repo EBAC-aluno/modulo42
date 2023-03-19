12a2150c71bc4a551420f05068b34e3285368cec
loboph <lobophf@gmail.com>
2023-03-19 14:26:57 -0300

    add gatway

---
+++ app-gatway/src/main/java/dev/pedrolobo/appgatway/AppGatwayApplication.java
@@ -0,0 +1,13 @@
+package dev.pedrolobo.appgatway;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AppGatwayApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AppGatwayApplication.class, args);
+	}
+
+}
---
+++ app-gatway/src/test/java/dev/pedrolobo/appgatway/AppGatwayApplicationTests.java
@@ -0,0 +1,13 @@
+package dev.pedrolobo.appgatway;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AppGatwayApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
