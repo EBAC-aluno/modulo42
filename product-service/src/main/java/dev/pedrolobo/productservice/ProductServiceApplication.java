4896462efa8e3ab5df677d9a008bf7b3ee4b684b
loboph <lobophf@gmail.com>
2023-03-19 12:18:18 -0300

    update

--- product-service/src/main/java/dev/pedrolobo/productservice/ProductServiceApplication.java
+++ product-service/src/main/java/dev/pedrolobo/productservice/ProductServiceApplication.java
@@ -1,5 +1,4 @@
 package dev.pedrolobo.productservice;
-
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
@@ -9,5 +8,4 @@ public class ProductServiceApplication {
 	public static void main(String[] args) {
 		SpringApplication.run(ProductServiceApplication.class, args);
 	}
-
 }
